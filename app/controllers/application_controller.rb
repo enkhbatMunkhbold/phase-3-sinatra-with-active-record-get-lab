8f801ac30372775d0336cb94fe486efc6c650646
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2023-01-17 11:56:57 -0800

    commit tree e891306c41bed8352f4234f664fb118170c01f10

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,25 @@
-class ApplicationController < Sinatra::Base
 
-  # add routes
+class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
+
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    baked_good = BakedGood.all.order('price DESC')
+    baked_good.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    baked_good = BakedGood.all.order('price DESC')[0]
+    baked_good.to_json(only: [:name, :price])
+  end
 
 end
